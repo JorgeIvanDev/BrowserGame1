dae0ffde0b1781614622d817e58c45be80518984
jorge <ivanjorge88@gmail.com>
2022-04-24 13:57:17 -0500

    stuck on boundaries

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const KEY_RIGHT = 39;
 const KEY_LEFT = 37;
 
 //DECLARING HEIGHT DIMENSIONS FOR GAME WINDOW//
-const GAME_WIDTH = 800;
+const GAME_WIDTH = 820; //changed dimension from 800 to allow for character to return//
 const GAME_HEIGHT = 600;
 
 //STORES KEY PARAMETERS OF GAME//(POSITION OF GAME CHARACTTER)//
@@ -28,11 +28,19 @@ function setSize($element, width) {
     $element.style.height = "auto";
   }
 
+function bound(x){
+    if (x <= 0){
+        STATE.x_pos = 0;
+        return STATE.x_pos
+      } else {
+        return x;
+      }
+    }
 
 
 // Player/Character
 function createPlayer($container) {
-    STATE.x_pos = GAME_WIDTH / 2;
+    STATE.x_pos = GAME_WIDTH / 2.35;
     STATE.y_pos = GAME_HEIGHT - 150;
     const $player = document.createElement("img");
     $player.src = "images/character.png";
@@ -44,12 +52,12 @@ function createPlayer($container) {
 
 function updatePlayer(){
     if(STATE.move_left){
-        STATE.x_pos -= 10;
+        STATE.x_pos -= 5;
     } if(STATE.move_right){
-        STATE.y_pos += 10;
+        STATE.x_pos += 5;
     } 
     const $player = document.querySelector(".player");
-    setPosition($player, STATE.x_pos, STATE.y_pos);
+    setPosition($player, bound(STATE.x_pos), STATE.y_pos);
 }
 
 
@@ -59,10 +67,10 @@ function updatePlayer(){
 
 function KeyPress(event) {
     if (event.keyCode === KEY_RIGHT) {
-      STATE.move_right = true;
+        STATE.move_right = true;
       console.log("right key is pressed")
     } else if (event.keyCode === KEY_LEFT) {
-      STATE.move_left = true;
+        STATE.move_left = true;
       console.log("left key is pressed")
     }
   }
